25c6cbc0a2e383561404ce983e7b6d7955dfdd2d
DangerNate8 <nathans02@vt.edu>
2025-05-31 18:01:48 -0400

    Desinend and implemented CreatePage that connects to Backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
+import { useColorModeValue } from "@/components/ui/color-mode"
 
 // Mentions where the code is for the different routes
 import CreatePage from './pages/CreatePage';
 import HomePage from './pages/HomePage';
 import Navbar from './components/ui/Navbar';
 
-function App() {
 
+function App() {
   return (
     <>
-    <Box minH={'100vh'}>
+    <Box minH={'100vh'} bgGradient="to-r" gradientFrom="red.200" gradientTo="blue.200" bg ={useColorModeValue("orange.100", "orange.200")}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
--- frontend/src/components/ui/Navbar.jsx
+++ frontend/src/components/ui/Navbar.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom"
-import { Container, Flex, Text, HStack } from "@chakra-ui/react"
+import { Container, Flex, Text, HStack, Button } from "@chakra-ui/react"
 import { IconButton } from "@chakra-ui/react"
 import { LuPlus } from "react-icons/lu"
+import { useColorMode, useColorModeValue } from "@/components/ui/color-mode"
 
 
 const Navbar = () => {
+  const { toggleColorMode } = useColorMode()
+  const icon = useColorModeValue("🌚", "🌞")
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex
@@ -18,22 +21,26 @@ const Navbar = () => {
        >
 
         <Text
-          fontSize={{base: "22", sm: "28"}}
+          fontSize={{base: "22px", sm: "28px"}}
           fontWeight={"bold"}
           textTransform={"uppercase"}
           textAlign={"center"}
-          bgGradient={"linear(to-r, cyan.400, blue.500)"}
-          bgClip={"text"}
+          bgGradient="to-r" gradientFrom="green.300" gradientTo="blue.400"
+          bgClip="text"
         >
-          <Link to={"/"}>Product Store </Link>
+            {/* Allows user to click to return to HomePage */}
+          <Link to={"/"}>Product Store 🛒</Link>    
         </Text>
 
         <HStack spacing={2} alignItems={"center"}>
           <Link to={"/create"}>
-          <IconButton aria-label="Search database" size={"sm"}>
+          <IconButton aria-label="Search database" size={"lg"}>
             <LuPlus />
           </IconButton>
           </Link>
+          <Button onClick={toggleColorMode}>
+            {icon}
+          </ Button>
         </HStack>
       </Flex>
     </Container>
